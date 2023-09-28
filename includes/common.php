43f62e9f41bb14ae335b598bd8d1f316f6839b15
mrlihx <mrlihx@163.com>
2023-09-28 15:55:26 +0800

    update

--- includes/common.php
+++ includes/common.php
@@ -17,6 +17,7 @@ if (!isset($nosession) || !$nosession) session_start();
 
 if(!$_SESSION['ref_uid']){
     $_SESSION['ref_uid'] = $_GET['ref'];
+    $ref_uid = $_SESSION['ref_uid'];
 }else{
     $ref_uid = $_SESSION['ref_uid'];
 }
