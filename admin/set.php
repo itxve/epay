4940d7e5e5d5bb644ca887a1b95ea1a04babaa51
mrlihx <mrlihx@163.com>
2023-11-03 08:41:33 +0800

    update

--- admin/set.php
+++ admin/set.php
@@ -1304,7 +1304,7 @@ elseif($mod=='proxy'){
     <hr>
       <div class="form-group">
           <label class="col-sm-2 control-label">URL代理</label>
-          <div class="col-sm-10"><input type="text" name="proxy_url" value="<?php echo $conf['proxy_url']; ?>" class="form-control" placeholder="第一次异步通知失败后会使用此方式重新通知一次 https://proxy.hostport.top/" /></div>
+          <div class="col-sm-10"><input type="text" name="proxy_url" value="<?php echo $conf['proxy_url']; ?>" class="form-control" placeholder="留空则不开启, URL结尾必须加/" /></div>
       </div><br/>
 	<div class="form-group">
 	  <div class="col-sm-offset-2 col-sm-10"><input type="submit" name="submit" value="修改" class="btn btn-primary btn-block"/><br/>
